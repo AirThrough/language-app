308349d75f256e59178e9d1d39f82a43ee797363
Andrey <andrey@insercle.com>
2023-10-22 14:54:27 +0400

    Initialized repository with the basic structure of react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+
+  // Main Page
+  // Actions: My Decks, My Cards, Create Deck, Create Card
+
+  return (
+      <div>
+          test
+      </div>
+  )
+}
+
+export default App
